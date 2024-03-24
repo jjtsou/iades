a678d6361a4d532700a3922a1ecbc2e7fcb40381
jjtsou <itsourtis@gmail.com>
2024-03-24 14:14:10 +0200

    remove unused prop

--- src/app/components/Navbar/constants.tsx
+++ src/app/components/Navbar/constants.tsx
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid"
 import { NavItem } from "./types"
 
 export const NAV_ITEMS: NavItem[] = [
-  { label: "Home", icon: <IoHomeOutline title="home" color="" size="20" />, id: uuidv4() },
+  { label: "Home", icon: <IoHomeOutline title="home" size="20" />, id: uuidv4() },
   {
     label: "About Us",
     icon: <IoInformationCircleOutline title="About Us" size="20" />,
