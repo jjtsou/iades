811f8dfade05de1c175b1fa8b5fcc5f8f9fbe475
jjtsou <itsourtis@gmail.com>
2024-03-25 13:55:18 +0200

    Introduce Footer Component and Layout Component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { Providers } from "./providers"
+import Navbar from "./components/Navbar"
+import { ReactNode } from "react"
+import { Flex } from "@chakra-ui/react"
+import Footer from "./components/Footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -12,13 +16,27 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: ReactNode
 }>) {
   return (
     <html lang="en" className={inter.className}>
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          <Layout>{children}</Layout>
+        </Providers>
       </body>
     </html>
   )
 }
+
+const Layout = ({
+  children,
+}: Readonly<{
+  children: ReactNode
+}>) => (
+  <Flex direction="column" justifyContent="center" alignItems="center">
+    <Navbar />
+    {children}
+    <Footer />
+  </Flex>
+)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 import { Flex } from "@chakra-ui/react"
 import Card from "./components/Card"
-import Navbar from "./components/Navbar"
 import { properties } from "./constants"
 
 export default function Home() {
   return (
-    <>
-      <Navbar />
-      <Flex direction="column" p={50} w="auto" h="auto" gap={10} alignItems="center">
-        {properties.map(property => (
-          <Card key={property.id} property={property} />
-        ))}
-      </Flex>
-    </>
+    <Flex direction="column" p={50} w="auto" h="auto" gap={10} alignItems="center">
+      {properties.map(property => (
+        <Card key={property.id} property={property} />
+      ))}
+    </Flex>
   )
 }
