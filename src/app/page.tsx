7c4eafc598ab1f269674efbc1c2ec9f162ac2a8f
John Tsourtis <itsourtis@gmail.com>
2024-04-07 12:28:53 +0300

    Introduce features Section (#8)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,38 @@
 "use client"
 
-import { Box, Flex } from "@chakra-ui/react"
+import { Box, Flex, Heading, Text } from "@chakra-ui/react"
 import Slider from "./components/Slider"
+import Features from "./components/Feature/features"
 
 export default function Home() {
   return (
-    <Flex direction="column" alignItems="center" gap={10}>
-      <Box>
+    <Flex direction="column" alignItems="center" gap={10} marginY={5}>
+      <Box as="section">
+        <Flex direction="column" justify="center" alignItems="center" gap={2} mb={2}>
+          <Text color="var(--chakra-colors-whatsapp-600)" fontWeight="bold">
+            Luxury Room Collection
+          </Text>
+          <Heading color="gray.600">Rooms & Suites</Heading>
+          <Text color="gray.600" maxW="30em" textAlign="center" marginX={5}>
+            Discover the allure of our exquisite Rooms & Suites, where luxury meets comfort and
+            every detail is meticulously crafted for your indulgence.
+          </Text>
+        </Flex>
         <Slider />
+      </Box>
+      <Box as="section">
+        <Flex direction="column" justify="center" alignItems="center" gap={2} mb={8}>
+          <Text color="var(--chakra-colors-whatsapp-600)" fontWeight="bold">
+            Our Exceptional Services
+          </Text>
+          <Heading color="gray.600">Discover the Finest Hotel Amenities</Heading>
+          <Text color="gray.600" maxW="30em" textAlign="center" marginX={5}>
+            Experience the epitome of hospitality with our comprehensive range of services. From
+            personalized assistance to exquisite amenities, we ensure your stay is nothing short of
+            extraordinary.
+          </Text>
+        </Flex>
+        <Features />
       </Box>
     </Flex>
   )
