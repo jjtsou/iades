61341c8efe16e6e5811f343689e59fdcfa14ad0a
John Tsourtis <itsourtis@gmail.com>
2024-06-01 20:41:18 +0300

    Introduce about us page and Hero component (#10)
    * Introduce about us page
    
    * Refactor hero component

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,121 @@
+"use client"
+
+import { SimpleGrid, Flex, chakra, Text, Box } from "@chakra-ui/react"
+import Image from "next/image"
+import imgNaxos from "/public/images/naxos/naxos11.jpg"
+
+const AboutUs = () => (
+  <Flex direction="column">
+    <SimpleGrid
+      columns={{
+        base: 1,
+        md: 2,
+      }}
+    >
+      <Flex
+        direction="column"
+        alignItems="start"
+        justifyContent="start"
+        px={{
+          base: 4,
+          lg: 20,
+        }}
+        py={24}
+      >
+        <chakra.h1
+          mb={6}
+          fontSize={{
+            base: "4xl",
+            md: "4xl",
+            lg: "5xl",
+          }}
+          fontWeight="bold"
+          color="brand.600"
+          lineHeight="shorter"
+        >
+          About Us
+        </chakra.h1>
+        <Text
+          pr={{
+            base: 0,
+            lg: 16,
+          }}
+          mb={4}
+          color="brand.600"
+          letterSpacing="wider"
+        >
+          Located on the enchanting island of Naxos, Iades Studios offers a tranquil haven for
+          travelers seeking serenity and relaxation amidst the stunning landscapes of the Aegean
+          Sea. Our boutique accommodation is nestled in Agia Anna, providing guests with convenient
+          access to Naxos' pristine beaches and vibrant culture.
+        </Text>
+        <Text
+          pr={{
+            base: 0,
+            lg: 16,
+          }}
+          mb={4}
+          color="brand.600"
+          letterSpacing="wider"
+        >
+          At Iades Studios, we pride ourselves on delivering an authentic Greek island experience
+          with a personal touch. As a family-operated establishment, we extend the warm hospitality
+          that Naxos is renowned for, ensuring that every guest feels at home from the moment they
+          arrive.
+        </Text>
+        <Text
+          pr={{
+            base: 0,
+            lg: 16,
+          }}
+          mb={4}
+          color="brand.600"
+          letterSpacing="wider"
+        >
+          Whether you're looking for a cozy retreat for a romantic escape or comfortable lodging for
+          a family vacation, we offer a range of options to suit your preferences. In addition to
+          our inviting accommodations, guests at Iades Studios can enjoy a variety of amenities and
+          services designed to enhance their stay.
+        </Text>
+        <Text
+          pr={{
+            base: 0,
+            lg: 16,
+          }}
+          mb={4}
+          color="brand.600"
+          letterSpacing="wider"
+        >
+          As advocates for sustainable tourism, we are committed to minimizing our environmental
+          impact and supporting the local community. We source locally produced goods for our studio
+          provisions and implement eco-conscious practices throughout our property to preserve
+          Naxos' natural beauty for future generations to enjoy.Whether you're embarking on a
+          romantic getaway, a family holiday, or an adventure-filled retreat, we invite you to
+          experience the tranquility and charm of Iades Studios in Naxos. We look forward to hosting
+          you and creating cherished memories together.
+        </Text>
+      </Flex>
+      <Box>
+        <Image src={imgNaxos} />
+      </Box>
+    </SimpleGrid>
+  </Flex>
+)
+
+export default AboutUs
+
+{
+  /* <Box
+      padding={6}
+      w="80%"
+      mx="auto"
+      bg="green.50"
+      sx={{ columnCount: [1, 2, 3], columnGap: "8px" }}
+    >
+      {naxosImages.map((src, i) => (
+        <Box overflow="hidden" borderRadius="xl" mb={2}>
+          <Image key={i} src={src} alt={`Naxos image ${i}`} />
+        </Box>
+      ))}
+    </Box> */
+}
--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -6,16 +6,7 @@ import { FaFacebookSquare } from "react-icons/fa"
 import { TbBrandBooking } from "react-icons/tb"
 
 const Footer = () => (
-  <Flex
-    w="full"
-    bg="green.50"
-    _dark={{
-      bg: "#3e3e3e",
-    }}
-    p={10}
-    alignItems="center"
-    justifyContent="center"
-  >
+  <Flex w="full" bg="green.50" p={10} alignItems="center" justifyContent="center">
     <Flex
       w="full"
       as="footer"
@@ -28,21 +19,8 @@ const Footer = () => (
       px="6"
       py="4"
       bg="white"
-      _dark={{
-        bg: "gray.800",
-      }}
     >
-      <Text
-        fontSize="xl"
-        fontWeight="bold"
-        color="gray.600"
-        _dark={{
-          color: "white",
-          _hover: {
-            color: "gray.300",
-          },
-        }}
-      >
+      <Text fontSize="xl" fontWeight="bold" color="gray.600">
         Iades
       </Text>
 
@@ -52,9 +30,6 @@ const Footer = () => (
           sm: "0",
         }}
         color="gray.800"
-        _dark={{
-          color: "white",
-        }}
       >
         All rights reserved
       </Box>
@@ -66,12 +41,6 @@ const Footer = () => (
           rel="noreferrer"
           mx="2"
           color="gray.600"
-          _dark={{
-            color: "gray.300",
-            _hover: {
-              color: "gray.400",
-            },
-          }}
           _hover={{
             color: "var(--chakra-colors-whatsapp-600)",
           }}
@@ -84,12 +53,6 @@ const Footer = () => (
           rel="noreferrer"
           mx="1"
           color="gray.600"
-          _dark={{
-            color: "gray.300",
-            _hover: {
-              color: "gray.400",
-            },
-          }}
           _hover={{
             color: "var(--chakra-colors-whatsapp-600)",
           }}
