cd64dac0ad4bb04aaf966bbdebc2165ddc902954
jjtsou <itsourtis@gmail.com>
2024-06-19 20:05:38 +0300

    remove logs

--- src/app/contact-us/components/ContactForm/index.tsx
+++ src/app/contact-us/components/ContactForm/index.tsx
@@ -21,7 +21,7 @@ const ContactForm = () => {
 
   const sendEmail: FormEventHandler<HTMLDivElement> = e => {
     e.preventDefault()
-    console.log(serviceId, templateId, publicKey)
+
     emailjs
       .sendForm(serviceId!, templateId!, ref.current!, {
         publicKey,
