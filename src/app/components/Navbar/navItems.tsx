2c6685a1db5e9783cc0aa238cf99f1958117ce6c
jjtsou <itsourtis@gmail.com>
2025-02-08 00:58:49 +0200

    fix: pass isExternal prop to navItem

--- src/app/components/Navbar/navItems.tsx
+++ src/app/components/Navbar/navItems.tsx
@@ -2,8 +2,15 @@ import { nav_items } from "../../constants"
 import NavItem from "./navItem"
 
 const NavItems = ({ onClick }: { onClick?: () => void }) =>
-  nav_items.map(({ label, href, icon, id }) => (
-    <NavItem key={id} label={label} href={href} icon={icon} onClick={onClick} />
+  nav_items.map(({ label, href, isExternal, icon, id }) => (
+    <NavItem
+      key={id}
+      label={label}
+      href={href}
+      icon={icon}
+      isExternal={isExternal}
+      onClick={onClick}
+    />
   ))
 
 export default NavItems
